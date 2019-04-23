8e4ce048542c89040e4eceb3fa309a955ec0226b
lizamarmysh <liza.marmysh1999@gmail.com>
2019-04-24 00:02:42 +0300

    commit 3

--- myapp/src/main/java/Main.java
+++ myapp/src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main {
         gift1.add(type2);
         gift1.add(type3);
 
-        log.info(gift1.toString());     //hkjh ddd
+        log.info(gift1.toString());     //hkjh dddsd
         System.out.println("\n------------------------Reverse By Name----------------------------");
         reverseByName(gift1, "Lolipop", type4);
         log.info(gift1.toString());
